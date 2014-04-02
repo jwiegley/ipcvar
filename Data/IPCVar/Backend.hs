5b4c6a717665be4d2cb6608443a58e28455d0e27
John Wiegley <johnw@newartisans.com>
2014-04-01 20:03:35 -0500

    Allow encoding/decoding of IPCVars themselves

--- Data/IPCVar/Backend.hs
+++ Data/IPCVar/Backend.hs
@@ -3,12 +3,14 @@
 module Data.IPCVar.Backend where
 
 import Data.Binary
+import Data.ByteString.Lazy
 
 data IPCVarBackend a = Binary a => IPCVarBackend
     { readValue   :: IO a
     , writeValue  :: a -> IO ()
     , swapValue   :: a -> IO a
     , deleteValue :: IO ()
+    , encodeState :: ByteString
     }
 
 newtype IPCVar a = IPCVar { getIPCBackend :: IPCVarBackend a }
--- Data/IPCVar/File.hs
+++ Data/IPCVar/File.hs
@@ -1,11 +1,15 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Data.IPCVar.File where
 
 import Control.Applicative
 import Control.Exception
 import Control.Monad
 import Data.Binary
-import Data.ByteString.Lazy.Char8
+import Data.ByteString.Lazy.Char8 as BS
 import Data.IPCVar.Backend
+import Data.Text.Lazy as T
+import Data.Text.Lazy.Encoding
 import System.Directory
 import System.IO hiding (hGetContents)
 import System.Posix.Files
@@ -22,14 +26,14 @@ withLock path mode req f = do
             `finally` waitToSetLock fd (Unlock, AbsoluteSeek, 0, 0)
 
 encodeFd :: Binary a => Fd -> a -> IO ()
-encodeFd fd x = void $ fdWrite fd (unpack (encode x))
+encodeFd fd x = void $ fdWrite fd (BS.unpack (encode x))
 
 decodeFd :: Binary a => Fd -> IO a
 decodeFd fd = do
     sz <- fdSeek fd SeekFromEnd 0 -- seek to the end to get the size
     _ <- fdSeek fd AbsoluteSeek 0
     (bs, sz') <- fdRead fd (fromIntegral sz)
-    assert (sz == fromIntegral sz') $ return $ decode (pack bs)
+    assert (sz == fromIntegral sz') $ return $ decode (BS.pack bs)
 
 fileIPCBackend :: Binary a => FilePath -> IPCVarBackend a
 fileIPCBackend path = IPCVarBackend
@@ -38,7 +42,15 @@ fileIPCBackend path = IPCVarBackend
     , swapValue  = \x -> withLock path ReadWrite WriteLock $ \fd ->
         decodeFd fd <* encodeFd fd x
     , deleteValue = removeFile path
+    , encodeState = encodeUtf8 (T.pack path)
     }
+
+decodeState :: Binary a => ByteString -> IPCVarBackend a
+decodeState = fileIPCBackend . T.unpack . decodeUtf8
+
+instance Binary a => Binary (IPCVar a) where
+    put (IPCVar b) = put (encodeState b)
+    get = IPCVar . decodeState <$> get
 
 newIPCVar :: Binary a => a -> IO (IPCVar a)
 newIPCVar x = do
