1eb04a080d82f48c8017cc9234584ea7323fc580
John Wiegley <johnw@newartisans.com>
2014-04-01 19:42:14 -0500

    Use file descriptors in File.hs

--- Data/IPCVar/File.hs
+++ Data/IPCVar/File.hs
@@ -1,34 +1,49 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Data.IPCVar.File where
 
 import Control.Applicative
 import Control.Exception
+import Control.Monad
 import Data.Binary
+import Data.ByteString.Lazy.Char8
 import Data.IPCVar.Backend
 import System.Directory
 import System.IO hiding (hGetContents)
+import System.Posix.Files
 import System.Posix.IO
+import System.Posix.Types
 
-withLock :: FilePath -> IOMode -> LockRequest -> IO a -> IO a
-withLock path mode req f = withBinaryFile path mode $ \h -> do
-    fd <- handleToFd h
-    (waitToSetLock fd (req, AbsoluteSeek, 0, 0) >> f)
-        `finally` waitToSetLock fd (Unlock, AbsoluteSeek, 0, 0)
+withLock :: FilePath -> OpenMode -> LockRequest -> (Fd -> IO a) -> IO a
+withLock path mode req f = do
+    fd <- openFd path mode (Just ownerModes) defaultFileFlags
+    go fd `finally` closeFd fd
+  where
+    go fd =
+        (waitToSetLock fd (req, AbsoluteSeek, 0, 0) >> f fd)
+            `finally` waitToSetLock fd (Unlock, AbsoluteSeek, 0, 0)
+
+encodeFd :: Binary a => Fd -> a -> IO ()
+encodeFd fd x = void $ fdWrite fd (unpack (encode x))
+
+decodeFd :: Binary a => Fd -> IO a
+decodeFd fd = do
+    sz <- fdSeek fd SeekFromEnd 0 -- seek to the end to get the size
+    _ <- fdSeek fd AbsoluteSeek 0
+    (bs, sz') <- fdRead fd (fromIntegral sz)
+    assert (sz == fromIntegral sz') $ return $ decode (pack bs)
 
 fileIPCBackend :: Binary a => FilePath -> IPCVarBackend a
 fileIPCBackend path = IPCVarBackend
-    { readValue  = withLock path ReadMode ReadLock (decodeFile path)
-    , writeValue = withLock path WriteMode WriteLock . encodeFile path
-    , swapValue  = \x -> withLock path ReadWriteMode WriteLock $
-        decodeFile path <* encodeFile path x
+    { readValue  = withLock path ReadOnly ReadLock decodeFd
+    , writeValue = \x -> withLock path WriteOnly WriteLock $ flip encodeFd x
+    , swapValue  = \x -> withLock path ReadWrite WriteLock $ \fd ->
+        decodeFd fd <* encodeFd fd x
     , deleteValue = removeFile path
     }
 
 newIPCVar :: Binary a => a -> IO (IPCVar a)
 newIPCVar x = do
     tmpDir <- getTemporaryDirectory
-    (path, h) <- openTempFile tmpDir "shmvar."
+    (path, h) <- openBinaryTempFile tmpDir "shmvar."
     go path h `onException` removeFile path
   where
     go path h = do
